41a05d37557b6ef74506b404fe8fe14702a11e16
Benjamin Linard <benjamin.linard@inrae.fr>
2026-04-22 15:32:50 +0200

    changed lon long to uint64 and put vectors as data structures in the matrix

---
+++ include/Parsers.h
@@ -0,0 +1,14 @@
+//
+// Created by belinard on 22/04/26.
+//
+
+#ifndef TFM_PVALUE_PARSERS_H
+#define TFM_PVALUE_PARSERS_H
+
+
+class Parsers {
+
+};
+
+
+#endif //TFM_PVALUE_PARSERS_H
---
+++ src/Parsers.cpp
@@ -0,0 +1,5 @@
+//
+// Created by belinard on 22/04/26.
+//
+
+#include "Parsers.h"
